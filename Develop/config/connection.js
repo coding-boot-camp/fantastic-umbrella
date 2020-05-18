9171986ab28999b071f26d2854555bda68e27bba
Xandromus <xanderrapstine@gmail.com>
2020-05-18 14:45:38 -0500

    changing env constants to match module

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
