3f68af811de2e2f7f049eaf122a3701994920b0c
CandiceRee <rebeccaeechols@aol.com>
2021-11-22 00:01:22 -0600

    Update Product.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name:{
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    price:{
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate:{
+        isDecimal:true
+      }
+    },
+    stock:{
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      defaultValue:10,
+      Validate:{
+        isNumeric: true
+      }
+    },
+    category_id:{
+      type: DataTypes.INTEGER,
+      references:{
+        model:'category',
+        key:'id'
+      }
+    }
   },
   {
     sequelize,
