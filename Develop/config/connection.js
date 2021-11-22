9bd298b64fa33716ea04928deba8c25764bb0371
CandiceRee <rebeccaeechols@aol.com>
2021-11-22 00:01:08 -0600

    Update connection.js

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -3,13 +3,15 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+if (process.env.JAWSDB_URL){
+  sequelize= new Sequelize(process.env.JAWSDB_URL);
+}else{
+  sequelize=new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
     });
-
+  }
 module.exports = sequelize;
