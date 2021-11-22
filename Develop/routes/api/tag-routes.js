58a966e583c4c724c94682a8000eeb7a32e119fd
CandiceRee <rebeccaeechols@aol.com>
2021-11-22 00:01:47 -0600

    Update tag-routes.js

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,19 +6,63 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+Tag.findAll({
+  include:{
+    model:Product,
+    attributes:['product_name','price','stock','category_id']
+  }
+})
+.then(dbTagData=>res.json(dbTagData))
+.catch(err=>{
+  res.status(500).json(err);
+});
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+Tag.findOne({
+  where:{
+    id:req.params.id
+  },
+  include:{
+    model:Product,
+    attributes:['product_name','price','stock','category_id']
+  }
+})
+.then(dbTagData=>res.json(dbTagData))
+.catch(err=>{
+  console.log(err);
+  res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name:req.body.tag_name
+  })
+  .then(dbTagData=>res.json(dbTagData))
+  .catch(err=>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+Tag.update(req.body, =>{
+  where:{
+    id:req.params.id
+  }
+})
+.then(dbTagData=>{
+  if(!dbTagData){
+    res.status(404).json({message:'No tag found with this id'});
+    return;
+  }
+  res.json(dbTagData);
+})
 });
 
 router.delete('/:id', (req, res) => {
