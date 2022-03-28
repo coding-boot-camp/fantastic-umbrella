716e7eb1e7d2dc66ecfc4dc5e1d89e97fe4529aa
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-03-27 23:26:50 -0400

    additional updates to routes

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,7 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
-
-/* const sequelize = process.env.JAWSDB_URL
+const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
@@ -17,6 +11,6 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
       dialectOptions: {
         decimalNumbers: true,
       },
-    }); */
-
+    });
+console.log(sequelize);
 module.exports = sequelize;
