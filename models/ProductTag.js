efd3c917eae0dc1349e849e544a7e90d085d867e
Hannahbird <hannahbrownson18@gmail.com>
2022-11-05 18:16:31 -0400

    producttag models added

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,15 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, allowNull: false, promaryKey: true, autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER, references: { model: 'tag', id: 'id' }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER, references: { model: 'tag', id: 'id' }
+    }
   },
   {
     sequelize,
