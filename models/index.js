2de3004074e1ccbe27c0006e37239a0e93715a66
Hannahbird <hannahbrownson18@gmail.com>
2022-11-05 18:27:59 -0400

    index models added

--- models/index.js
+++ models/index.js
@@ -5,13 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {foreignKey: 'category_id'})
 // Categories have many Products
-
+Category.hasMany(Product, {foreignKey: 'category_id'})
+ProductTag.belongsTo(Product, {foreignKey: 'product_id'})
+ProductTag.belongsTo(Tag, {foreignKey: 'tag_id'})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, { through: ProductTag, as: 'product_tags'})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, { through: ProductTag, as: 'tagged_products'})
 module.exports = {
   Product,
   Category,
