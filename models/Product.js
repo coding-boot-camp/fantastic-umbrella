321d526ff1a24fa4162ae380d1e1a8f899d4c0c1
Hannahbird <hannahbrownson18@gmail.com>
2022-11-05 18:10:59 -0400

    product models added

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,21 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING, allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL, allowNull: false, validate: { isDecimal: true }
+    },
+    stock: {
+      type: DataTypes.INTEGER, allowNull: false, defaultValue: 10, validate: { isNumeric: true }
+    },
+    category_id: {
+      type: DataTypes.INTEGER, references: { model: 'category', id: 'id'}
+    }
   },
   {
     sequelize,
