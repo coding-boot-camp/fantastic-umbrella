657346f851d20255c7f87eb6bc9c4dd44f3e50c4
Hannahbird <hannahbrownson18@gmail.com>
2022-11-05 18:19:35 -0400

    tag models added

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,12 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING, allowNull: false
+    }
   },
   {
     sequelize,
