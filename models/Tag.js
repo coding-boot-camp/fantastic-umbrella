ccd2f58d35abd0099b31d728a5b924d90c5dc43a
marysgreenwood <marysgreenwood@gmail.com>
2022-12-11 12:55:48 -0500

    defined columns in Tag model

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,15 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
