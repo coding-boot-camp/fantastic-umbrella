0d9be474cb99be9b1b2927e66e472c5b2b13ff09
marysgreenwood <marysgreenwood@gmail.com>
2022-12-11 12:40:32 -0500

    defined columns for Category model

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,17 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+
   },
   {
     sequelize,
