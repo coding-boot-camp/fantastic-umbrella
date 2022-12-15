d9616a736d2a6e50f17c61ea6fed91dd438e4b47
marysgreenwood <marysgreenwood@gmail.com>
2022-12-15 12:52:37 -0500

    updated models/index.js

--- models/index.js
+++ models/index.js
@@ -6,20 +6,20 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
-})
+  foreignKey: 'category_id',
+});
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id'
-})
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  through: ProductTag
-})
+Product.belongsToMany (Tags, {
+  through: ProductTag,
+});
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
-  through: ProductTag
-})
+Tag.belongsToMany (Product, {
+  through: ProductTag,
+});
 module.exports = {
   Product,
   Category,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,16 +4,41 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const allProducts = await Product.findAll({
+      // be sure to include its associated Category and Tag data
+      include: [{ model: Category}, { model: Tag }],
+    });
+    res.status(200).json(allProducts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
+router.get('/:id', async (req, res) => {
+  try {
+    // find a single product by its `id`
+    const productData = await Product.findByPk(req.params.id, {
+      // be sure to include its associated Category and Tag data
+      include: [{ model: Category }, { model: Tag }],
+  });
+  
+  if (!productData) {
+        res.status(404).json({ message: 'Product not found' });
+        return;
+      }
+  
+      res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  
+
 
 // create new product
 router.post('/', (req, res) => {
@@ -22,7 +47,7 @@ router.post('/', (req, res) => {
       product_name: "Basketball",
       price: 200.00,
       stock: 3,
-      tagIds: [1, 2, 3, 4]
+      categoryIds: [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
