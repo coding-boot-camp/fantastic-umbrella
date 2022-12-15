904b0f7cde1040926bdf7303921713e81d0734a2
marysgreenwood <marysgreenwood@gmail.com>
2022-12-15 18:01:01 -0500

    corrected typos in models and updated README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# E-commerce Back End Starter Code
+# E-commerce Database Interface
+
+This application allows users to interface with online product inventory. Users can view, create, update or delete products, categories and tags. 
+
+This software requires node js, mysql, dotenv and sequelize to run. Be sure to install necessary dependencies and create dotenv file with mysql login credentials.
+
+Use mysql to create the database before running seeds/index.js to populate database.
+
+VIDEO LINK HERE
+
+LICENSE GOES HERE
--- models/index.js
+++ models/index.js
@@ -4,16 +4,17 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-});
+
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany (Tags, {
+Product.belongsToMany (Tag, {
   through: ProductTag,
 });
 // Tags belongToMany Products (through ProductTag)
