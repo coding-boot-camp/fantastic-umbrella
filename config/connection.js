5860f3905bfedf77aba7ee715b73cf0567b67988
Koro999 <chris.dang93@hotmail.com>
2023-07-18 16:50:35 -0400

    Databased seeded and synced

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,23 @@
+const mysql = require('mysql2')
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  : new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
+    {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
-    });
+      port: 3306
+    },
+    console.log(`Connected to ${process.env.DB_NAME} database.`)
+  );
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+  console.log(`App listening on port https://localhost:${PORT}!`);
 });
